d5eb5c753959ad41ff30c6f39cfe5eef3f23dc7b
snakedata <62944294+snakedata@users.noreply.github.com>
2023-05-13 13:55:22 +0200

    Update first_project.py

--- first_project.py
+++ first_project.py
@@ -1,57 +1,5 @@
 import re 
-
-class State:
-    """This class contains information about the state
-    """
-    def __init__(self, name:str):
-        self.name = name
-
-class States:
-    """_summary_
-
-    Returns:
-        _type_: _description_
-    """
-    def __init__(self):
-        pass
     
-    def __iadd__(self, next:State):
-        
-        return self
-    
-    def __len__(self):
-        """_summary_
-        """
-    def __iter__(self):
-        """Returns 
-        """
-    def __next__(self):
-        """
-        """
-
-class Transition:
-    """This class contains the information about actions between states
-    """
-    def __init__(self, from_state: State, probability:int, to_state: State):
-        """Any action is identified by a name which is a string. The probability of that action is used in the MDP computation 
-
-        Args:
-            name (str): identifier of the action
-            prob (float): probability of the action """
-        self.from_state = from_state
-       
-        self.to_state = to_state
-        self.probability = probability
-    def __getstate1__(self):
-        return self.from_state
-    def __getstate2__(self):
-        return self.to_state
-    def __getprobability__(self):
-        return self.probability
-    
-
-
-
 def parse(file: str)-> str:
 
     """Given a file it returns any strings that match the format given
@@ -107,20 +55,10 @@ def Bellman_Equation(costs:list,probabilities:list):
                 cond = False
             Vi[v]=minimum(sum)
             sum = 0
-           
+    return Vi       
 
 
     
-def optimal_policy(state):
-    """compute the optimal policy
-    """
-    for i in range():
-        pass
-   
-def compute():
-    """Compute the data
-    """
-
 def get(lines:list):    
     final = []
 
@@ -184,14 +122,7 @@ def create_matrix(lines:list):
     return matrix
 
 
-        
-
-
-            
-            
-            
-    
 lines =  parse("input.txt")
 separated_lines = get(lines)
 matrix = create_matrix(separated_lines)
-print(matrix)
+Bellman_Equation(matrix)
