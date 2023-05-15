12988bf964280d289b80dff99403a9d4a2c89acd
juanma6610 <115793176+juanma6610@users.noreply.github.com>
2023-05-15 20:04:15 +0200

    Update Create_file.py

--- Create_file.py
+++ Create_file.py
@@ -13,24 +13,24 @@ def createtxt(states:list, actions:list)->None:
         for p in range(0,3):
             f.write("state"+add_zeros(str(states[i]))+"-"+str(actions[0])+"-"+"state"+add_zeros(str(int(states[i])+int(state2[p])))+":"+str(proboff[p]))
             f.write("\n")
-    f.write("state15-0-state15:90\n")                ##state15=16ºC state16=24.5ºC state17=25ºC
-    f.write("state15-0-state00:10\n")
-    f.write("state16-0-state16:20\n")
-    f.write("state16-0-state14:70\n")               ##Manually writing the operations that are exceptions
-    f.write("state16-0-state17:10\n")
-    f.write("state17-0-state17:30\n")
-    f.write("state17-0-state16:70\n")
-    f.write("state00-0-state15:70\n")
+    f.write("state16-0-state16:90\n")                
+    f.write("state16-0-state00:10\n")                   ##state16=16ºC state17=24.5ºC state18=25ºC
+    f.write("state17-0-state17:20\n")
+    f.write("state17-0-state15:70\n")               ##Manually writing the operations that are exceptions
+    f.write("state17-0-state18:10\n")
+    f.write("state18-0-state18:30\n")
+    f.write("state18-0-state17:70\n")
+    f.write("state00-0-state16:70\n")
     for i in range(len(states)):     ##For loop to write the operations involving the thermostat on
         for p in range(0,4):
             f.write("state"+add_zeros(str(states[i]))+"-"+str(actions[1])+"-"+"state"+add_zeros(str(int(states[i])+int(state2[p])))+":"+str(probon[p]))
             f.write("\n")
-    f.write("state15-1-state15:30\n")
-    f.write("state15-1-state00:50\n")
-    f.write("state15-1-state01:20\n")
-    f.write("state16-1-state16:20\n")
-    f.write("state16-1-state17:70\n")           ##Manually writing the operations that are exceptions
-    f.write("state16-1-state14:10\n")
-    f.write("state17-1-state17:90\n")
-    f.write("state17-1-state16:10\n")
-    f.write("state00-1-state15:10\n")
+    f.write("state16-1-state16:30\n")
+    f.write("state16-1-state00:50\n")
+    f.write("state16-1-state01:20\n")
+    f.write("state17-1-state17:20\n")
+    f.write("state17-1-state18:70\n")           ##Manually writing the operations that are exceptions
+    f.write("state17-1-state15:10\n")
+    f.write("state18-1-state18:90\n")
+    f.write("state18-1-state17:10\n")
+    f.write("state00-1-state16:10\n")
